184ac1143a161ebb28bd45fa26815ca7c80690be
Ashim Das <98227937+Ashim-das123@users.noreply.github.com>
2022-06-08 23:33:40 +0530

    Add files via upload

---
+++ arr_reverse.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+// void reverse(int arr[], int n)
+// {
+//     int s = 0;
+//     int e = n - 1;
+//     while (s < e)
+//     {
+//         swap(arr[s], arr[e]);
+//         s++;
+//         e--;
+//     }
+// }
+// void print(int arr[], int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] <<" ";
+//     }
+    
+// }
+
+int main()
+{
+    // int n;
+    // cin >> n;
+    // int arr[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+
+    // reverse(arr, n);
+
+    // print(arr, n);
+
+    // 2nd approch
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int s =0;
+    int e = n-1;
+    while(s<e){
+        swap(arr[s],arr[e]);
+        s++;e--;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+    return 0;
+}
